5f3ef1fcc7c9122b7f2c771b32ed32e0066f5f06
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-16 10:51:10 +0200

    live coding

---
+++ javascript_full/function/index.js
@@ -0,0 +1,29 @@
+function getSenseOfLife () {
+return 42
+}
+// const res = getSenseOfLife();
+// console.log(res);
+console.log(getSenseOfLife());
+
+
+
+function getSquare(num) {
+  return num * num;
+}
+console.log(getSquare());
+
+console.log(getSquare(4));
+console.log(getSquare(-50));
+console.log(getSquare(0));
+
+
+function printMassage (years) {
+  console.log('I am '+ years + ' years old');
+}
+
+console.log(printMassage(421));
+console.log(printMassage(21));
+console.log(printMassage(51));
+
+
+let mult = numb, numb 
