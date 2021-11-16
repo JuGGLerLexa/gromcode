b212b517498a8b01fea7bf3c1bdbca3cfee42b4c
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-16 20:46:26 +0200

    it's hard

---
+++ javascript_full/loops/lesson_3/index.js
@@ -0,0 +1,7 @@
+for (let firstNum = 1; firstNum <= 5; firstNum++) {
+  console.log("\n " + firstNum + " <=");
+
+  for (let secondNum = 1; secondNum <= 10; secondNum++) {
+    console.log(firstNum + " x " + secondNum + " = " + firstNum * secondNum);
+  }
+}
