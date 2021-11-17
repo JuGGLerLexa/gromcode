64f9bb86e26948dec9d683e03b90948938b3798e
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-17 11:07:47 +0200

    live_cod

--- javascript_full/loops/home_2/index.js
+++ javascript_full/loops/home_2/index.js
@@ -1,6 +1,6 @@
 // put your code here
 let n = 21;
-let sum = ' ';
+let sum = '';
 // стрінга + число, конкатинація..
 for (let i = 1; i <= n; i++) {
     sum += i
---
+++ live_coding_6/index.js
@@ -0,0 +1,18 @@
+// How to work on tech tasks. Step by step guide
+
+// 1. Learn requirement
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
+
+// algo
+// 1. iterete numbers from 2 tp num
+// 2. iterete from 2 to 1
+// 3. 
+
+
+// input = number
+// output = undefained
+function (num) {
+
+}
