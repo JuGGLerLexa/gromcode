69505cf3b46d084e00e24ae89e2db06a1097aa57
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-18 18:23:56 +0200

    learn

---
+++ javascript_full/lesson_5/lesson_1/index.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} firstNum
+ * @param {number} secondNum
+ * @return {number}
+ */
+function sum(firstNum, secondNum) {
+  // put your code here
+  const sum = firstNum + secondNum;
+  return sum;
+}
+
+// examples
+sum(11, 11); // ==> 22
+sum(-2, 10); // ==> 8
+
+// 2
+
+//put your code here
+let hola = 'Hi, ';
+function sayHi(name) {
+  const say = hola + name;
+  console.log(say);
+}
+
+sayHi('Lexa');
+
+// 3
+//put your code here
+function getEvenOdd(num) {
+  if (num % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+}
