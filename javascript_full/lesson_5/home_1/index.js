eb0659cc9801b2754bf57dab1891d18409db52e6
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-18 22:34:27 +0200

    всьо пливу вже ...

---
+++ javascript_full/lesson_5/home_1/index.js
@@ -0,0 +1,22 @@
+// 1. Learn requirement +++
+// 2. Create step by step algo (& input/output for funtions) +++
+// 3. Write draft solution & testing +++
+// 4. Refactoring & final testing -> final solution +++
+
+// ціле число
+// function getSum(firstNum, secondNum) {
+//   let i = firstNum;
+//   while (i <= secondNum) {
+
+//   }
+//   console.log(getSum)
+// }
+
+// Боже.. як я все люблю ускладнювати..
+function getSum(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+console.log(getSum(12.21, 21));
+console.log(getSum(1421, 213));
+console.log(getSum(12345, 154321));
