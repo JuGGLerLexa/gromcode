4a16b648a90c7fd217ec3f974468efe6694ded51
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-19 23:34:34 +0200

    2 tack done

--- javascript_full/lesson_6/lesson_2/app.js
+++ javascript_full/lesson_6/lesson_2/app.js
@@ -6,10 +6,17 @@
  */
 function getSum(arr) {
   // put your code here
+  let sum = 0;
+  // if (arr === []) {
+    if (Array.isArray(arr)) {
+    for (const num of arr) sum += num;
+    return sum;
+  }
+  return null;
 }
 
 // examples
-getSum([1, 10, -10, 4]); // ==> 5
-getSum([1]); // ==> 1
-getSum([-8, 8]); // ==> 0
-getSum(10, 12, 14); // ==> null
+console.log(getSum([1, 10, -10, 4])); // ==> 5
+console.log(getSum([1])); // ==> 1
+console.log(getSum([-8, 8])); // ==> 0
+console.log(getSum(10, 12, 14)); // ==> null
