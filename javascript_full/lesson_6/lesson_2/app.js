46805dd03b4f9c1884b04614eb2aeffc7c4861ba
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-19 19:42:07 +0200

    must go..

---
+++ javascript_full/lesson_6/lesson_2/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function getSum(arr) {
+  // put your code here
+}
+
+// examples
+getSum([1, 10, -10, 4]); // ==> 5
+getSum([1]); // ==> 1
+getSum([-8, 8]); // ==> 0
+getSum(10, 12, 14); // ==> null
