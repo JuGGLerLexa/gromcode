1b9d35ddc9bb950cd4aadbfa112e36fc7d721736
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-20 14:54:47 +0200

    correct

--- javascript_full/lesson_6/lesson_3/index.js
+++ javascript_full/lesson_6/lesson_3/index.js
@@ -4,16 +4,30 @@
  * @param {number} from
  * @param {number} to
  * @return {number[]}
- */
+//  */
+// function getSpecialNumbers(from, to) {
+//   // put your code here
+//   // if (!Array.isArray(from, to)) return
+//   const specialNumbers = [];
+//   for (let i = from; i <= to; i += 1) {
+//     if (i % 3 === 0) specialNumbers.push(i);
+//   }
+//   return specialNumbers;
+// }
+
 function getSpecialNumbers(from, to) {
-  // put your code here
-  // if (!Array.isArray(from, to)) return
-  const newArr = [];
+  const specialNumber = [];
+
   for (let i = from; i <= to; i += 1) {
-    if (i % 3 === 0) newArr.push(i);
+    if (i % 3 === 0) {
+      specialNumber.pop(i);
+    }
   }
-  return newArr;
+  return specialNumber;
 }
+const specialNumber = getSpecialNumbers(10, 30);
+
+console.log(specialNumber);
 // examples
 console.log(getSpecialNumbers(1, 10)); // ==> [3, 6, 9]
 console.log(getSpecialNumbers(-10, 10)); // ==> [-9, -6, -3, 0, 3, 6, 9]
---
+++ javascript_full/lesson_6/lesson_4/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @param {number[]} numbers
+ * @return {number[]}
+ */
+function swap(numbers) {
+  // put your code here
+}
+
+/**
+ * @param {number[]} numbers
+ * @return {number[]}
+ */
+function swapManual(numbers) {
+  // put your code here
+}
+
+// examples
+swap([1, 10, 9, 11]); // ==> [10, 9, 11, 1]
+swapManual([1, 10, 9, 11]); // ==> [10, 9, 11, 1]
