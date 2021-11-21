fa9c276d87d19850348ce8eae7425e27ef5c57af
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-21 14:55:13 +0200

    complite tasck 3

---
+++ javascript_full/lesson_6/home_3/app.js
@@ -0,0 +1,14 @@
+const checkSum = (arr) => {
+  if (!Array.isArray(arr)) return null;
+
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i += 1) sum += arr[i];
+
+  return sum > 100;
+};
+
+// tests
+console.log(checkSum(412));
+console.log(checkSum([412]));
+console.log(checkSum([311]));
