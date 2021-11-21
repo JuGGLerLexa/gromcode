aaf47a456d0cf89557dc8132d043e8337e57cb1f
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-21 14:00:30 +0200

    complite tasck 2

--- javascript_full/lesson_6/home_2/index.js
+++ javascript_full/lesson_6/home_2/index.js
@@ -1,15 +1,27 @@
 'use strict';
 
+// cheack list
+//
+// якщо не масив = null
+// якщо масив = *
+//
+//
 /**
  * @param {number[]} arr
  * @return {number[]}
  */
 function squareArray(arr) {
   // put your code here
-  return arr * arr;
+  const squareNum = [];
+  if (!Array.isArray(arr)) return null;
+  for (let i = 0; i < arr.length; i += 1) {
+    squareNum.push(arr[i] * arr[i]);
+  }
+  return squareNum;
 }
 
 // examples
-squareArray([1, 10, 9, 11]); // ==> [1, 100, 81, 121]
-squareArray([10, 0, -4]); // ==> [100, 0, 16]
-squareArray([1]); // ==> [1]
+console.log(squareArray([1, 10, 9, 11])); // ==> [1, 100, 81, 121]
+console.log(squareArray([10, 0, -4])); // ==> [100, 0, 16]
+console.log(squareArray([1])); // ==> [1]
+console.log(squareArray(12)); // ==> null
