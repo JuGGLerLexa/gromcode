583ef4cb35cd980d164a152d9f278c2416fc0629
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-21 17:50:33 +0200

    bed cod

--- javascript_full/lesson_6/home_4/index.js
+++ javascript_full/lesson_6/home_4/index.js
@@ -7,12 +7,16 @@
  */
 const increaseEvenEl = (arr, delta) => {
   // put your code here
-  const sum = 0;
+  const sum = [];
   if (!Array.isArray === arr) return null;
-  // for (let i = arr; arr)
+  for (const i of arr) {
+    if (i % 2 === 0) sum.push(i + delta);
+    sum.push(i);
+  }
+  return sum;
 };
 
 // examples
 console.log(increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20)); // ===> [22, 5, 26, 28, 11, 9, 24]
-increaseEvenEl([7, 11, 1], 10); // ===> [7, 11, 1]
-increaseEvenEl([], 120); // ===> []
+console.log(increaseEvenEl([7, 11, 1], 10)); // ===> [7, 11, 1]
+console.log(increaseEvenEl([], 120)); // ===> []
