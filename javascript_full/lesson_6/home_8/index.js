1c4fc87c9813e203ebc2fad58024cc58f026cbb5
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-22 21:04:22 +0200

    arrays done

--- javascript_full/lesson_6/home_8/index.js
+++ javascript_full/lesson_6/home_8/index.js
@@ -1,14 +1,49 @@
-//put your code here
+/* eslint-disable no-param-reassign */
+// put your code here
 // input: arr
 // outinput: arr (newArr, sort)
 // я вражений якщо чесно :/
 
-function sortAss(array) {
-  if (!Array.isArray) return null;
-  let increases = [];
-  for ()
-};
-  
-  function sortDesc(array) {
-  
+// put your code here
+// [...twoDay] 2 дні з цією задачою возився
+
+function sortAsc(array) {
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        const tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
   }
+
+  return array;
+}
+
+function sortDesc(array) {
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < array.length; i += 1) {
+      if (array[i - 1] < array[i]) {
+        done = false;
+        const tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const numbers = [12, 10, 15, 11, 14, 13, 16];
+const numbers1 = [12, 10, 15, 11, 14, 13, 16];
+sortAsc(numbers);
+console.log(numbers);
+sortDesc(numbers1);
+console.log(numbers1);
