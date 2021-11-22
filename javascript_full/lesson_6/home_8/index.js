fc6116bbf08335dd8c7158e23d3925d451c16e75
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-22 16:02:22 +0200

    skip

---
+++ javascript_full/lesson_6/home_8/index.js
@@ -0,0 +1,14 @@
+//put your code here
+// input: arr
+// outinput: arr (newArr, sort)
+// я вражений якщо чесно :/
+
+function sortAss(array) {
+  if (!Array.isArray) return null;
+  let increases = [];
+  for ()
+};
+  
+  function sortDesc(array) {
+  
+  }
