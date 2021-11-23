90bc2a7f07bc2acefbdc43472e108cfa734d6b1d
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-23 23:48:19 +0200

    It's not work! It's just test

---
+++ javascript_full/lesson_7/home_1/index.js
@@ -0,0 +1,12 @@
+//put your code here
+//input: arr
+// out: (double)Arr
+
+// It's not work! It's just test
+const squareArray = arr => {
+  if (!Array.isArray) return null;
+  const doubles = arr.map(function(num) {
+return doubles * 2;
+)};
+
+// console.log(doubles);
