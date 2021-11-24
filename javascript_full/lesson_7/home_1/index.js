a0c7166d7c6eaad1b75b6984dd4726da2a779022
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-24 12:26:25 +0200

    done

--- javascript_full/lesson_7/home_1/index.js
+++ javascript_full/lesson_7/home_1/index.js
@@ -1,12 +1,17 @@
-//put your code here
-//input: arr
-// out: (double)Arr
+// put your code here
+// input: arr
+// out: (double)Arr (if not => null)
 
 // It's not work! It's just test
 const squareArray = arr => {
-  if (!Array.isArray) return null;
-  const doubles = arr.map(function(num) {
-return doubles * 2;
-)};
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  return arr.map(double => double * double);
+};
 
-// console.log(doubles);
+// test data
+const test = [1, 0, 21, 101, 0.4, 5, true];
+const test1 = (1, 0, 21, 101, 0.4, 5, true);
+console.log(squareArray(test));
+console.log(squareArray(test1));
