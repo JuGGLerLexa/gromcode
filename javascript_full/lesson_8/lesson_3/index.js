ed169798ec1289b2c24c90dcb0985c0089a77a1e
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-29 22:53:01 +0200

    done

---
+++ javascript_full/lesson_8/lesson_3/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+function getKeys(obj) {
+  // put you code here
+  for (const key in obj) {
+    console.log(getKeys);
+  }
+}
+
+const obj = {
+  name: 'lexa',
+  age: 21,
+};
+
+console.log(obj);
