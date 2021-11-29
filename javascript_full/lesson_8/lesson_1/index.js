f3857cbd8e78d5e8432877665837b4647d3b4116
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-29 22:31:15 +0200

    done

---
+++ javascript_full/lesson_8/lesson_1/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable dot-notation */
+const transaction = {
+  currency: 'USD',
+  value: 170,
+  operation: 'sale',
+  agent: {
+    country: 'Ukraine',
+    company: 'NYSE',
+  },
+  'operation time': 1584029990001,
+};
+
+/* выведи в консоль значение свойства 'currency' с помощью точки */
+console.log(transaction.currency);
+/* выведи в консоль значение свойства 'value' с помощью квадратных скобок */
+console.log(transaction['value']);
+const key = 'operation';
+/* выведи в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
+console.log(transaction[key]);
+/* выведи в консоль значение свойства 'company' используя квадратные скобки */
+console.log(transaction['agent']['company']);
+/* выведи в консоль значение свойства 'country' используя точку */
+console.log(transaction.agent.country);
+/* выведи в консоль значение свойства 'operation time' используя квадратные скобки */
+console.log(transaction['operation time']);
