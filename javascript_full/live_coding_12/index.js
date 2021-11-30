3baf67585b11d1b4beebbbef6529d8409e912659
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-30 09:35:29 +0200

    start

---
+++ javascript_full/live_coding_12/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @param {string[]} keysList
+ * @param {array} valuesList
+ * @return {object}
+ */
+function buildObject(keysList, valuesList) {
+  // put you code here
+}
+
+// examples
+const keys = ['name', 'address', 'age'];
+const values = ['Bob', 'Ukraine', 34];
+const result = buildObject(keys, values); // ==> { name: 'Bob', address: 'Ukraine', age: 34 }
