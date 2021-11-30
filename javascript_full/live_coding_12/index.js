f28bf1c834b44212f77afea1dd3c3841ad66ec4d
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-30 10:31:26 +0200

    task

--- javascript_full/live_coding_12/index.js
+++ javascript_full/live_coding_12/index.js
@@ -1,15 +1,26 @@
-'use strict';
+// input: arr, arr
+// output: obj
 
-/**
- * @param {string[]} keysList
- * @param {array} valuesList
- * @return {object}
- */
+// algo
+// 1. creat empty object
+// 2. iterate arrays & put key/value to the obj
+
+// BAD (for)
+// function buildObject(keysList, valuesList) {
+//   const res = {};
+//   for (let index = 0; index < keysList.length; index += 1) {
+//     res[keysList[index]] = valuesList[index];
+//   }
+
+//   return res;
+// }
+
+// GOOD
 function buildObject(keysList, valuesList) {
-  // put you code here
+  return keysList.reduce((acc, key, index) => ({ ...acc, [key]: valuesList[index] }), {});
 }
-
 // examples
 const keys = ['name', 'address', 'age'];
 const values = ['Bob', 'Ukraine', 34];
 const result = buildObject(keys, values); // ==> { name: 'Bob', address: 'Ukraine', age: 34 }
+console.log(buildObject(keys, values));
---
+++ javascript_full/live_coding_12/task_3/index.js
@@ -0,0 +1,37 @@
+// input: obj1, obj2
+// out: bool
+
+// algo
+// 1. compare length, if not equal -> false
+// 2. iterate obj1 keys
+// 3. compare value1 != value2 -> false
+
+function compareObjects(obj1, obj2) {
+  // put your code here
+}
+
+// examples
+const obj1 = {
+  name: 'Tom',
+  age: 17,
+};
+
+const obj2 = {
+  name: 'Bob',
+  age: 17,
+};
+
+const obj3 = {
+  name: 'Bob',
+  age: 17,
+  student: false,
+};
+
+const obj4 = {
+  name: 'Tom',
+  age: 17,
+};
+
+compareObjects(obj1, obj2); // ==> false
+compareObjects(obj2, obj3); // ==> false
+compareObjects(obj1, obj4); // ==> true
