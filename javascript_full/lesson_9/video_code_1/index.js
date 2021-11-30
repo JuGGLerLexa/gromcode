d78360df6083cf19a6dbb52a96cc0a1637bed639
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-11-30 23:31:46 +0200

    awesome refactoring and method obj

--- javascript_full/lesson_9/video_code_1/index.js
+++ javascript_full/lesson_9/video_code_1/index.js
@@ -1,12 +1,44 @@
 // getAdults
 
+// const user = {
+//   name: 'Tom',
+//   age: 17,
+// };
+
+// Object.keys();
+// Object.values();
+// Object.entries();
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
 const user = {
-  name: 'Tom',
-  key: 17,
-}
+  Tom: 17,
+  'John Doe': 19,
+  Bob: 18,
+};
 
-Object.keys();
-Object.values();
-Object.entries();
+// const getAdults = usersObj => {
+//   // { 'Tom': 17, 'John Doe': 19, } => [ ['Tom', 17], ['John Doe', 19] ]
+//   const usersArray = Object.entries(usersObj);
 
-console.log(object.keys(user));
+//   // [ ['Tom', 17], ['John Doe', 19] ] => [ ['John Doe', 19] ]
+//   const filteredUsersArray = usersArray.filter(user => user[1] >= 18);
+
+//   // [ ['John Doe', 19] ] => ['John Doe']
+//   const usersNames = filteredUsersArray.map(user => user[0]);
+
+//   return usersNames;
+// };
+
+// console.log(getAdults(user)); /* 'John Doe', 'Bob' */
+
+// refactoring
+
+const getAdults = usersObj =>
+  Object.entries(usersObj)
+    .filter(user => user[1] >= 18)
+    .map(user => user[0]);
+
+console.log(getAdults(user)); /* 'John Doe', 'Bob' */
