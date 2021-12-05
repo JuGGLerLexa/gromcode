5eddbdc6f21f908a410b07035f3194c4a478fbf0
JuGGLerLexa <lexa.klipach2000@gmail.com>
2021-12-05 22:34:31 +0200

    new

--- live_coding/live_coding_8/index.js
+++ live_coding/live_coding_8/index.js
@@ -1,126 +1,74 @@
-/* ==================================== */
-/* ===> мутирующие методы массивов <=== */
-/* ===> изменяют исходный массив <===== */
-/* ==================================== */
-
-const numbersList = [1, 2, 3, 4, 5];
-
-// input: num
-// outinput: num
-
-// const res = numbersList.push('sdaas');
-// numbersList[numbersList.length] = 222;
-// console.log(res);
-
-// console.log(numbersList);
-/* pop - удалит из массива последний элемент и вернет его */
-
-// c помощью метода pop достань последний элемент массива numbersList1 и помести его в переменную lastNumber
-// выведи в консоль эту переменную, а так же массив numbersList1 после выполнения pop
-
-const numbersList1 = [1, 2, 3, 4, 5];
-
-// put your code here
-// input: callback;
-// outinput: arr
-
-// const anotherNumbersList.filter();
-/* push - добавит элемент в конец массива */
-
-// c помощью метода push добавь число 6 в конец массива numbersList2
-// выведи в консоль результат работы метода push, а так же массив numbersList2 после выполнения push
-
-const numbersList2 = [1, 2, 3, 4, 5];
-
-// put your code here
-// input: none
-// output: num
-console.log(numbersList2.shift);
-
-/* shift - удалит из массива первый элемент и вернет его */
-
-// c помощью метода shift достань первый элемент массива numbersList3 и помести его в переменную firstNumber
-// выведи в консоль эту переменную, а так же массив numbersList2 после выполнения shift
-
-const numbersList3 = [1, 2, 3, 4, 5];
-
-// put your code here
-
-/* unshift - добавит элемент в начало массива */
-
-// c помощью метода unshift добавь число 0 в начало массива
-// выведи в консоль результат работы метода unshift, а так же массив numbersList4 после выполнения unshift
-
-const numbersList4 = [1, 2, 3, 4, 5];
-
-// put your code here
-
-/* ======================================= */
-/* ===> НЕ мутирующие методы массивов <=== */
-/* ===> НЕ изменяют исходный массив <===== */
-/* ======================================= */
-
 // начальный массив
+const numbersList = [5, 0, 8, 10, -4, 50, 220];
+
+/* метод map */
+/* const newArr = arr.map(callback) - создает и возвращает новый массив. Длина нового и исходного массивов равны
+   ф-ция callback запустится по очереди для каждого элемента начального массива arr
+   элементы массива newArr - это результат вызова callback для каждого элемента arr
+   newArr[i] = callback(arr[i]) */
+
+// c помощью метода map создай новый массив squaredNumbers, в котором будут квадраты чисел из numbersList
+// выведи squaredNumbers в консоль
+
+// put your code here
+
+/* метод find */
+/* const arrElement = arr.find(callback) - вернет первый элемент массива arr, который удовлетворяет условию в callback.
+   ф-ция callback запустится по очереди для каждого элемента начального массива arr 
+   если callback(arr[i]) вернет true, то find прекратит поиск и вернет этот элемент */
+
+// c помощью метода find найди первое нечетное число в numbersList и выведи его в консоль
+
+// put your code here
+
+/* метод forEach */
+/* arr.forEach(callback) - итерирует массив и выполняет callback функцию по очереди для каждого элемента 
+начального массива arr. callback функция ничего не должна возвращать */
+
+// c помощью метода forEach выведи в консоль только положителные элементы массива numbersList
+
+// put your code here
+
+/* метод reduce */
+/* const newArr = arr.reduce(callback) - применяет функцию callback к каждому элементу массива (слева-направо), 
+возвращая одно результирующее значение. Функция callback принимает два параметра - accumulator, currentValue */
+
+// С помощью метода reduce посчитай сумму чисел массива transactions которые больше 100
+// выведи в консоль результат выполнения, а так же массив transactions после выполнения reduce
+
+const transactions = [5, 0, 8, 10, -4, 50, 220, 1203, 556, 41];
+
+// put your code here
+
+/* sort - отсортирует массив в заданом порядке */
+
+// c помощью метода sort отсортируй массив numbersList1 по убыванию, результат помести в переменную sortedArr1
+// выведи в консоль sortedArr1, а так же массив numbersList1 после выполнения sort
+
+const numbersList1 = [6, 20, 33, 43, 8];
+
+// put your code here
+
+// c помощью метода sort отсортируй массив numbersList2 по убыванию, но чтобы исходный массив не менялся.
+// результат помести в переменную sortedArr2
+// выведи в консоль sortedArr2, а так же массив numbersList2 после выполнения sort
+
+const numbersList2 = [6, 20, 33, 43, 8];
+
+// put your code here
 const anotherNumbersList = [5, 0, 8, 10, -4, 50, 220];
 
-/* метод filter */
-/* const newArr = arr.filter(callback) - создает и возвращает новый отфильтрованный массив.
-   ф-ция callback запустится по очереди для каждого элемента начального массива arr
-   в newArr попадут только те элементы arr, для которых callback(arr[i]) вернет true
-   callback(arr[i]) === true -> элемент будет добавлен в финальный массив
-   callback(arr[i]) === false -> элемент НЕ будет добавлен в финальный массив */
-
-// c помощью метода filter создай новый массив bigNumbers, в котором будут числа > 5 из anotherNumbersList
-// выведи bigNumbers в консоль
-
-// put your code here
-
-// c помощью метода filter создай новый массив evenPositions, в котором будут числа из четных позиций которые > 5
-// массива anotherNumbersList
-// выведи evenPositions в консоль
-
-// put your code here
-
-/* TASCK CALLBACK
- * A callback is a function passed as an argument to another function
- * This technique allows a function to call another function
- * A callback function can run after another function has finished
- * */
-// calback printer
-// input: num
-// out: undefined
-// function sum(from, to, printer) {
-//   let result = 0;
-//   for (let index = from; index <= to; index += 1) {
-//     result += index;
-//   }
-//   printer(result);
-//   // put your code here
-// }
-
-// // test data
-// const num1 = 5;
-// const num2 = 10;
-
-// function printResultExample(res) {
-//   // put your code here
-//   console.log('Result is ' + res);
-//   // alert(res);
-// }
-
-//
-//
-//
-//
-//
-//
-anotherNumbersList.filter();
-
-const filterRes = anotherNumbersList.filter(function (num) {
-  if (num > 10) {
-    return true;
-  }
-  return false;
-});
-
+const filterRes = anotherNumbersList.filter(num => num > 10);
+console.log('callback was called');
+// test data
 console.log(filterRes);
+
+// console.log(anotherNumbersList.filter => el > 10);
+// const filterRes = anotherNumbersList.filter((num, index, arr) => {
+//   console.log('callback was called');
+
+//   return num > 10;
+// });
+
+// test data
+// console.log(filterRes);
