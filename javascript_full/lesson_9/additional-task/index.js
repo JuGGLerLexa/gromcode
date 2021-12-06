d0b8c027c4d50385c6242648a9fa248fa640f10f
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2021-12-06 09:51:46 +0200

    I'm come back

--- javascript_full/lesson_9/additional-task/index.js
+++ javascript_full/lesson_9/additional-task/index.js
@@ -1,2 +1,37 @@
-console.log(123);
-const lexa = 'coming';
+// some(), reduce (non ideal)
+
+function compareObjects(obj1, obj2) {
+    const keys = Object.keys(obj1);
+  
+    if (keys.length !== Object.keys(obj2).length) {
+      return false;
+    }
+    return keys.every(key => obj1[key] === obj2[key]);
+  }
+  
+  // examples
+  const obj1 = {
+    name: 'Tom',
+    age: 17,
+  };
+  
+  const obj2 = {
+    name: 'Bob',
+    age: 17,
+  };
+  
+  const obj3 = {
+    name: 'Bob',
+    age: 17,
+    student: false,
+  };
+  
+  const obj4 = {
+    name: 'Tom',
+    age: 17,
+  };
+  
+  compareObjects(obj1, obj2); // ==> false
+  compareObjects(obj2, obj3); // ==> false
+  compareObjects(obj1, obj4); // ==> true
+  
