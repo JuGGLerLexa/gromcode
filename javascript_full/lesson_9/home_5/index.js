e5ee5def429a4e9a73da762968343d3aec8ecac9
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2021-12-09 17:19:08 +0200

    commit

---
+++ javascript_full/lesson_9/home_5/index.js
@@ -0,0 +1,32 @@
+const getTotalRevenue = transactions => {
+  const result = [];
+  const getAmount = transactions.amount;
+  return getAmount;
+  //   return result.map.Object.values(transactions);
+};
+
+// const getTotalRevenue = transactions => {
+//   const resultArr = [];
+
+//   transactions.forEach(element => {
+//     const get = element.amount;
+//     console.log(get);
+//     resultArr.push(get);
+//   });
+
+//   const result = resultArr.reduce((acc, add) => acc + add);
+
+//   return result;
+// };
+
+// examples
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
+
+const result = getTotalRevenue(dayTransactions); // ==> 310
+console.log(result);
+
+console.log(dayTransactions);
