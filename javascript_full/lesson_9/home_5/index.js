380e2d7b4ca725056e0ffcf94689f7e54c1a94ce
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2021-12-13 19:07:33 +0200

    refactoring and done

--- javascript_full/lesson_9/home_5/index.js
+++ javascript_full/lesson_9/home_5/index.js
@@ -1,19 +1,29 @@
 // input: num;
 // outinput: sumNum
 
+const getTotalRevenue = transactions =>
+  Object.values(transactions)
+    .flatMap(amount => amount.amount)
+    .reduce((acc, num) => acc + num, 0);
+
+// refactoring
+
 // const getTotalRevenue = transactions => {
-//   const arr = transactions.map(amount => Object.values(amount));
-//   // const sum = arr.filter(amount => amount + amount);
-//   return arr;
+//   const getRevenu = Object.values(transactions);
+//   console.log(getRevenu);
+
+//   const values = getRevenu.flatMap(amount => amount.amount);
+//   console.log(values);
+//   const sum = values.reduce((acc, num) => acc + num, 0);
+//   console.log(sum);
 // };
 
 // examples
-// const dayTransactions = [
-//   { userId: 22, amount: 60, operation: 'sell' },
-//   { userId: 22, amount: 160, operation: 'buy' },
-//   { userId: 44, amount: 90, operation: 'sell' },
-// ];
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
 
-// const result = getTotalRevenue(dayTransactions); // ==> 310
-
-// console.log(result);
+const result = getTotalRevenue(dayTransactions); // ==> 310
+console.log(result);
