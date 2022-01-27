a8297256811cf3f88630d9226e4ea9e04aa010dc
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-01-27 16:21:37 +0200

    done first

--- javascript_full/lesson_10/task1/index.js
+++ javascript_full/lesson_10/task1/index.js
@@ -1,4 +1,6 @@
 const getfiniteNumbers = arr => arr.filter(el => Number.isFinite(el));
+const random = [1, 2, 3.4, '123', 'abc', NaN, undefined, 56];
+console.log(getfiniteNumbers(random));
 
 const getfiniteNumbersV2 = arr => arr.filter(el => isFinite);
 
