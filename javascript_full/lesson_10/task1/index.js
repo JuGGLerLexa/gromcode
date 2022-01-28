c84943a07ec56600d3a593e7c53b8869eaa7a913
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-01-28 10:50:29 +0200

    correct

--- javascript_full/lesson_10/task1/index.js
+++ javascript_full/lesson_10/task1/index.js
@@ -1,6 +1,6 @@
-const getfiniteNumbers = arr => arr.filter(el => Number.isFinite(el));
+const getFiniteNumbers = arr => arr.filter(el => Number.isFinite(el));
 
-const getfiniteNumbersV2 = arr => arr.filter(el => isFinite(el));
+const getFiniteNumbersV2 = arr => arr.filter(el => isFinite(el));
 
 const getNaN = arr => arr.filter(el => Number.isNaN(el));
 
@@ -11,7 +11,7 @@ const getIntegers = arr => arr.filter(el => Number.isInteger(el));
 const random = [1, 2, 3.4, '123 ', 'abc', NaN, undefined, 56];
 const random1 = [123, 2142, '  2134', NaN, undefined, null];
 
-console.log(getfiniteNumbers(random));
+console.log(getFiniteNumbers(random));
 console.log(getIntegers(random1));
 
 const isEqualTo = getfiniteNumbers === getfiniteNumbersV2;
