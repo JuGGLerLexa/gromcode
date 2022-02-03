cbf352d9fac02e599ee5fd6ea74b62e2d1c2d3b3
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-03 13:29:06 +0200

    done

--- javascript_full/lesson_10/task4/script.js
+++ javascript_full/lesson_10/task4/script.js
@@ -1,13 +1,21 @@
-/**
- * @param {number[]} arr
- * @return {number}
- */
-const getMaxAbsoluteNumber = arr => {
-  // put your code here
-};
+function getMaxAbsoluteNumber(arr) {
+  if (!Array.isArray(arr) || arr.length < 1) {
+    // перевірка на масив
+    return null;
+  }
+  const array = arr.map(i => {
+    return Math.abs(i); // The Math.abs() method returns the absolute value of a number.
+  });
+  const result = Math.max(...array); // через декілька днів рефакторингу в гуглі натикаюсь і згадую за спред оператор :( "вік живи - вік вчись."
+  return result;
+}
 
 // examples
-getMaxAbsoluteNumber([-10, 10, -10]); // ===> 10
-getMaxAbsoluteNumber([2.1, 0, 1.6]); // ===> 2.1
-getMaxAbsoluteNumber([-6, 3, 5, -1]); // ===> 6
-getMaxAbsoluteNumber([-777, 3, -1, 45, -20]); // ===> 777
+console.log(getMaxAbsoluteNumber([-10, 10, -10])); // ===> 10
+console.log(getMaxAbsoluteNumber([2.1, 0, 1.6])); // ===> 2.1
+console.log(getMaxAbsoluteNumber([-6, 3, 5, -1])); // ===> 6
+console.log(getMaxAbsoluteNumber([-777, 3, -1, 45, -20])); // ===> 777
+
+const test = [0.2];
+
+console.log(test.length < 1);
