aa7441c928f074eb39fca8ed4c7f6fa08411baf4
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-06 12:12:26 +0200

    done

---
+++ javascript_full/lesson_10/task7/main.js
@@ -0,0 +1,18 @@
+const superRound = (num, prec) => {
+  // put your code here
+  const precPow = 10 ** prec;
+
+  const res = [
+    Math.floor(num * precPow) / precPow,
+    Math.trunc(num * precPow) / precPow,
+    Math.ceil(num * precPow) / precPow,
+    Math.round(num * precPow) / precPow,
+    +num.toFixed(prec),
+  ];
+
+  return res;
+};
+
+// examples
+console.log(superRound(11.12556, 2)); // ==> [11.12, 11.12, 11.13, 11.13, 11.13]
+console.log(superRound(6.11, 3)); // ==> [6.11, 6.11, 6.11, 6.11, 6.11]
