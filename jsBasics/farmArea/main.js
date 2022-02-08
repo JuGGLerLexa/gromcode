cd0210d021e7e7777e8e4211604ca80661ab790f
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-08 16:47:41 +0200

    flex time

---
+++ jsBasics/farmArea/main.js
@@ -0,0 +1,5 @@
+const countArea = (chickens, caws, pigs) => chickens * 0.2 + caws * 1.5 + pigs * 0.5;
+
+console.log(countArea(10, 10, 10)); // ===> 22
+console.log(countArea(10, 0, 1)); // ===> 2.5
+console.log(countArea(6, 3, 5)); // ===> 8.2
