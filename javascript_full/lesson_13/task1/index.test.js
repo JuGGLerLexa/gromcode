1ae54d47b2a8ed480cbdc11f2b63accc063a3478
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-21 21:06:31 +0200

    in progress and add git config in my email

--- javascript_full/lesson_13/task1/index.test.js
+++ javascript_full/lesson_13/task1/index.test.js
@@ -7,3 +7,11 @@ it('17 и в Африке 17', () => {
 it('18 это вам не 17', () => {
   expect(18).not.toEqual(17);
 });
+
+const getEvenNumbers = numbers => numbers.filter(num => num % 2 === 0);
+
+it('should get only even numbers from array', () => {
+  const result = getEvenNumbers([1, 2, 3, 4]);
+
+  expect(result).toEqual([2, 4]);
+});
