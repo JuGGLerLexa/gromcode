c7dd8ebcdd23528a9ba1cc989b5d41953f1cdd32
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-21 22:46:41 +0200

    add babelrc and have a failed error

---
+++ javascript_full/lesson_13/task2/calculator.js
@@ -0,0 +1,7 @@
+// separete code and tests
+
+const getSquaredNumbers = arr => arr.map(num => num * num);
+
+const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
+
+const sum = (a, b) => a + b;
