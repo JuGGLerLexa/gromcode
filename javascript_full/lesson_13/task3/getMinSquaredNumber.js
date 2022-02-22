0d9d8ba09b399660eadb4cf1985afa25f05c25a9
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-22 16:56:17 +0200

    in progress

---
+++ javascript_full/lesson_13/task3/getMinSquaredNumber.js
@@ -0,0 +1,13 @@
+// put your code here
+
+// getMinSquaredNumber.test.js
+
+// // [-777, 3, -2, 6, 45, -20] => 4
+// [] => return null;
+
+// | x |
+
+//     x === x
+
+// if (x !== -x) return null;
+// if (x === []) return null;
