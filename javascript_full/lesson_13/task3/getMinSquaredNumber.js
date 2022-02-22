1f4e0a42de28f34e7e53a33c018f9ef3a967ad43
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-02-22 22:36:24 +0200

    done, but not refactoring -__-

--- javascript_full/lesson_13/task3/getMinSquaredNumber.js
+++ javascript_full/lesson_13/task3/getMinSquaredNumber.js
@@ -1,8 +1,10 @@
 // put your code here
 
+// input: arr
+// out: num
+
 // getMinSquaredNumber.test.js
 
-// // [-777, 3, -2, 6, 45, -20] => 4
 // [] => return null;
 
 // | x |
@@ -11,12 +13,15 @@
 
 // if (x !== -x) return null;
 // if (x === []) return null;
-.
-.
 
-.
+// export default
+const defaultt = arr => {
+  if (!Array.isArray(arr)) return null;
+  if (arr.length === 0) return null;
+  const minMinSquare = Math.min(...arr.map(el => el * el));
 
+  console.log(minMinSquare);
+  return minMinSquare;
+};
 
-
-.
-.
+console.log(defaultt([-777, 3, -2, 6, 45, -20])); // => 4
