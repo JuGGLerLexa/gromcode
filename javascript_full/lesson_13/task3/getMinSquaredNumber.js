ae925790cfb251b64a7c0681bc85017ca0830600
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-03-09 13:45:20 +0200

    test

--- javascript_full/lesson_13/task3/getMinSquaredNumber.js
+++ javascript_full/lesson_13/task3/getMinSquaredNumber.js
@@ -1,27 +1,8 @@
-// put your code here
-
-// input: arr
-// out: num
-
-// getMinSquaredNumber.test.js
-
-// [] => return null;
-
-// | x |
-
-//     x === x
-
-// if (x !== -x) return null;
-// if (x === []) return null;
-
-// export default
-const defaultt = arr => {
-  if (!Array.isArray(arr)) return null;
-  if (arr.length === 0) return null;
+export default arr => {
+  if (!Array.isArray(arr) || arr.length === 0) return null;
   const minMinSquare = Math.min(...arr.map(el => el * el));
 
-  console.log(minMinSquare);
   return minMinSquare;
 };
 
-console.log(defaultt([-777, 3, -2, 6, 45, -20])); // => 4
+// console.log(default([-777, 3, -2, 6, 45, -20])); // => 4
---
+++ javascript_full/lesson_13/task3/getMinSquaredNumber.test.js
@@ -0,0 +1,5 @@
+import getMinSquaredNumber from './getMinSquaredNumber.js';
+
+it('should min num', () => {
+  const result = [-777, 3, -2, 6, 45, -20];
+});
