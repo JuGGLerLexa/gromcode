cb804322b70911aeb31234cf3e8cbe1ecf6fea41
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-03-26 23:01:26 +0200

    add jest for calculator

--- javascript_full/lesson_13/task4/calculator.js
+++ javascript_full/lesson_13/task4/calculator.js
@@ -1,9 +1,7 @@
 /* eslint-disable default-case */
 
 export const calc = expression => {
-  if (typeof expression !== 'string') {
-    return null;
-  }
+  if (typeof expression !== 'string') return null;
 
   const [a, operation, b] = expression.split(' ');
   let result;
@@ -25,3 +23,9 @@ export const calc = expression => {
 
   return `${expression} = ${result}`;
 };
+
+console.log(calc('5 + 2'));
+console.log(calc('5 * 2'));
+console.log(calc('49 / 7'));
+console.log(calc('5' + []));
+console.log(calc(7 + 7));
--- javascript_full/lesson_13/task4/calculator.test.js
+++ javascript_full/lesson_13/task4/calculator.test.js
@@ -1,3 +1,25 @@
-import calculator from './calculator.js';
+import { calc } from './calculator.js';
 
-it('')
+it('should plus', () => {
+  const resultFirst = calc('5 + 2');
+
+  expect(resultFirst).toEqual('5 + 2 = 7');
+});
+
+it('should minus', () => {
+  const resultSecond = calc('5 - 2');
+
+  expect(resultSecond).toEqual('5 - 2 = 3');
+});
+
+it('should multi', () => {
+  const resultTh = calc('5 * 2');
+
+  expect(resultTh).toEqual('5 * 2 = 10');
+});
+
+it('should /', () => {
+  const resultFo = calc('49 / 7');
+
+  expect(resultFo).toEqual('49 / 7 = 7');
+});
