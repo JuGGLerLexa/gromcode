3533b0d38d3f9593f6f0463ff4432689c1042841
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-03-27 23:51:45 +0300

    in progress

--- javascript_full/lesson_13/task5/index.js
+++ javascript_full/lesson_13/task5/index.js
@@ -1,6 +1,10 @@
-export const reverseArray = arr => {
-  if (!Array.isArray(arr)) return null;
+/* eslint-disable no-param-reassign */
+// task 1
 
+export const reverseArray = arr => {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
   return arr.reverse();
 };
 
@@ -14,3 +18,33 @@ console.log(reverseArray(test1));
 
 const test2 = (2, 3);
 console.log(reverseArray(test2));
+
+// task 2
+
+const withdraw = (clients, balances, client, amount) => {
+  const clientId = clients.indexOf(client);
+  if (balances[clientId] >= amount) {
+    // eslint-disable-next-line no-return-assign
+    return (balances[clientId] -= amount);
+  }
+  // console.log(balances);
+  return -1;
+};
+
+// example 1:
+
+// input
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50));
+
+// output
+// 37
+// и массив balances должен быть [1400, 37, -6]
+
+// example 2:
+
+// input
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10));
+
+// output
+// -1
+// и массив balances должен быть [1400, 87, -6]
---
+++ javascript_full/lesson_13/task5/index.test.js
@@ -0,0 +1,13 @@
+import { reverseArray } from './index.js';
+
+it('should reverse', () => {
+  const resultReverse = reverseArray([1, 2, 3, 4, 5]);
+
+  expect(resultReverse).toEqual([5, 4, 3, 2, 1]);
+});
+
+it('should reverse', () => {
+  const resultBalanse = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50));
+
+  expect(resultBalanse).toEqual([5, 4, 3, 2, 1]);
+});
