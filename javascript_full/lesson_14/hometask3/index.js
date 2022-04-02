60ebd691b065e25534dfdef682ff034cfc1ec59e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-02 13:24:28 +0300

    looking for video

--- javascript_full/lesson_14/hometask3/index.js
+++ javascript_full/lesson_14/hometask3/index.js
@@ -8,8 +8,8 @@ export function decrease(number) {
   remember -= number;
 }
 
-export function reset(number) {
-  remember = number * 0;
+export function reset() {
+  remember = 0;
 }
 
 export function getMemo() {
