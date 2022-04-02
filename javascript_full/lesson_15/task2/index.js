34a465454d571c10caf911f6040588fdbf2d5de1
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-02 13:47:16 +0300

    Сlosure

---
+++ javascript_full/lesson_15/task2/index.js
@@ -0,0 +1,15 @@
+const message = 'Have a good day!';
+
+function sendMessage(nema) {
+  const greeting = 'Hi, ';
+
+  if (name) {
+    const greeting = 'Hello, ';
+    const name = 'Tad';
+    console.log(greeting + name);
+  } else {
+    console.log(message);
+  }
+}
+
+sendMessage('Ann');
