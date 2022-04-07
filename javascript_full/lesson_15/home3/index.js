fa5edbb983ba5bcb093a3be4ec736d2c5c9d091e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-07 12:47:06 +0300

    refactoring

--- javascript_full/lesson_15/home3/index.js
+++ javascript_full/lesson_15/home3/index.js
@@ -1,15 +1,11 @@
-const createLogger = () => {
+export const createLogger = () => {
   const loggerArr = [];
 
-  const warn = str => {
-    loggerArr.push({ message: str, dateTime: new Date(), type: 'warn' });
-  };
-  function error(str) {
-    loggerArr.push({ message: str, dateTime: new Date(), type: 'error' });
-  }
-  function log(str) {
-    loggerArr.push({ message: str, dateTime: new Date(), type: 'log' });
-  }
+  const warn = str => loggerArr.push({ message: str, dateTime: new Date(), type: 'warn' });
+
+  const error = str => loggerArr.push({ message: str, dateTime: new Date(), type: 'error' });
+
+  const log = str => loggerArr.push({ message: str, dateTime: new Date(), type: 'log' });
 
   function getRecords(typeAll) {
     if (typeAll) {
@@ -25,18 +21,18 @@ const createLogger = () => {
 const logger1 = createLogger();
 
 console.log(logger1.log('User logged in'));
-logger1.warn('User is tring to ented restricted page');
-logger1.log('User logged out');
-logger1.error('Unexpected error on the site');
+console.log(logger1.warn('User is tring to ented restricted page'));
+console.log(logger1.log('User logged out'));
+console.log(logger1.error('Unexpected error on the site'));
 
-logger1.getRecords(); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-logger1.getRecords('log'); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+console.log(logger1.getRecords()); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+console.log(logger1.getRecords('log')); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
 logger1.getRecords('error'); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
 logger1.getRecords('warn'); // ===> [{ message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }]
 
 const logger2 = createLogger();
 logger2.warn('Opps, something is happenning');
-logger2.getRecords('error'); // ===> []
+console.log(logger2.getRecords('error')); // ===> []
 logger2.getRecords('warn'); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
 logger2.getRecords(); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
 
