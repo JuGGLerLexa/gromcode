9f10d08613242c18ee556609d807ca4f0260d2af
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-11 22:39:26 +0300

    use destructuring

--- javascript_full/lesson_17/h7/index.js
+++ javascript_full/lesson_17/h7/index.js
@@ -1,5 +1,14 @@
-export const user = {
-  getFullName() {
-    return `${firstName} ${lastName}`;
+const user = {
+  getFullName: function getFullName() {
+    return `${this.firstName} ${this.lastName}`;
   },
 };
+
+user.getFullName = function setFullName(name) {
+  const result = name.split(' ');
+  this.firstName = result[0];
+  this.lastName = result[1];
+  console.log(`${this.firstName} ${this.lastName}`);
+};
+
+user.getFullName('John Doe');
