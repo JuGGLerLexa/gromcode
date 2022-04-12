f6b7cd5999705b171f78a5ba25282bc7a6dfbbfb
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-12 23:33:02 +0300

    it's very bad

--- javascript_full/lesson_17/h7/index.js
+++ javascript_full/lesson_17/h7/index.js
@@ -1,15 +1,24 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable prefer-destructuring */
-const user = {
-  getFullName: function getFullName() {
+// algo:
+// 1. обєкт user дії відбувають в ньому чи ні?
+// 2. getFullName повинен ретьорнути імя
+// 3. setFullName повинен розбити getFullName на масив щоб зберігати окремо фімілію і імя по умові
+// використовуємо метод split
+
+export const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  getFullName() {
     return `${this.firstName} ${this.lastName}`;
+  },
+  setFullName(fullName) {
+    const data = fullName.split(' ');
+    // eslint-disable-next-line no-sequences
+    (this.firstName = data[0]), (this.lastName = data[1]);
   },
 };
 
-user.getFullName = function setFullName(name) {
-  const result = name.split(' ');
-  this.firstName = result[0];
-  this.lastName = result[1];
-  console.log(`${this.firstName} ${this.lastName}`);
-};
-
-user.getFullName('John Doe');
+console.log(user.getFullName());
+console.log(user.setFullName('Jim Carrey'));
+console.log(user.getFullName());
