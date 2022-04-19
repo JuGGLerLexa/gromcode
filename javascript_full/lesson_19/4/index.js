16d4d113e92e3070b686d929fde4d08b9383b34c
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-19 22:58:59 +0300

    test deta

---
+++ javascript_full/lesson_19/4/index.js
@@ -0,0 +1,14 @@
+export default {
+  firstName: 'John',
+  lastName: 'Doe',
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  },
+};
+
+// test deta
