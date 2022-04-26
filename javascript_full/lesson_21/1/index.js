911f85a94487bdd2762d7d0062b629b88efb3f83
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-26 21:38:40 +0300

    DOM

--- javascript_full/lesson_21/1/index.js
+++ javascript_full/lesson_21/1/index.js
@@ -1,18 +1,30 @@
-const tasks = ['Buy milk', 'Pickup up Tom from airport', 'Visit party', 'Buy meat'];
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pickup up Tom from airport', done: true },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: false },
+];
 
 const renderListItems = listItems => {
   const listItem = document.querySelector('.list');
 
-  const listItemsElems = listItems.map(itemText => {
-    const listItemsElem = document.createElement('li');
-    listItemsElem.classList.add('list__item');
-    const checkboxElem = document.createElement('input');
-    checkboxElem.setAttribute('type', 'checkbox');
-    checkboxElem.classList.add('list__item-checkbox');
-    listItemsElem.append(itemText);
+  const listItemsElems = listItems
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemsElem = document.createElement('li');
+      listItemsElem.classList.add('list__item');
+      if (done) {
+        listItemsElem.classList.add('list__item-checkbox');
+      }
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.checked = done;
+      checkboxElem.classList.add('list__item-checkbox');
+      listItemsElem.append(text);
 
-    return listItemsElem;
-  });
+      return listItemsElem;
+    });
 
   listItem.append(...listItemsElems);
 };
