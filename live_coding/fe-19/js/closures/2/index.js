1205f0062d8c52318f8069a36240739063be2beb
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-28 11:43:56 +0300

    closures

---
+++ live_coding/fe-19/js/closures/2/index.js
@@ -0,0 +1,42 @@
+// hoisting var example 1
+// var message
+// console.log(message);
+// message = 'Hoisting is here!';
+
+// console.log(message);
+// var message = 'Hoisting is here!';
+
+// var a = 55;
+// console.log(a);
+
+// if (a) {
+//   var a = 1;
+
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// hoisting var example 3
+// var a = 77;
+// console.log(a);
+
+// function print() {
+//   var a = 2;
+//   console.log(a);
+// }
+// print();
+
+// console.log(a);
+
+// hoisting func example 4
+// run();
+
+// function run() {
+//   console.log('RUN');
+// }
+
+// stop();
+// var stop = function () {
+//   console.log('STOP');
+// };
