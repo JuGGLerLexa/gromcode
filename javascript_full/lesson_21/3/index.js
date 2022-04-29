2b830134e59af1c7f04327fb15b44daab3de5ccc
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-29 21:28:06 +0300

    fix, ca

--- javascript_full/lesson_21/3/index.js
+++ javascript_full/lesson_21/3/index.js
@@ -7,7 +7,7 @@ export const getItemsList = () => {
 export const getItemsArray = () => {
   const elementsArray = Array.from(document.querySelectorAll('.tool'));
   console.dir(elementsArray);
-  return getItemsArray;
+  return elementsArray;
 };
 
 getItemsList();
