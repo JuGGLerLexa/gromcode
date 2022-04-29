8ad87106838583febbe6c14ad5c845c89343c9b6
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-29 21:12:23 +0300

    DOM all element

--- javascript_full/lesson_21/3/index.js
+++ javascript_full/lesson_21/3/index.js
@@ -1,9 +1,15 @@
 const getItemsList = () => {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
+  return elementsList;
 };
 
+getItemsList();
+
 const getItemsArray = () => {
-  const elementsArray = document.querySelectorAll();
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
   console.dir(elementsArray);
+  return getItemsArray;
 };
+
+getItemsArray();
