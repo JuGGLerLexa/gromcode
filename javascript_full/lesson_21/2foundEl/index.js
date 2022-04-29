ec4950efda2fadeecf0515f2c8e9ccc2ed2e1e66
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-29 16:47:31 +0300

    fix

--- javascript_full/lesson_21/2foundEl/index.js
+++ javascript_full/lesson_21/2foundEl/index.js
@@ -1,8 +1,11 @@
 const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.log(titleElem);
+  return titleElem;
 };
+
 const getInputElement = () => {
   const inputElem = document.querySelector('input[type=text]');
   console.dir(inputElem);
+  return inputElem;
 };
