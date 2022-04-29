6b302ba2bca6473dfcb399376f6d3ad7c0b2143a
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-04-29 15:49:51 +0300

    DOM

---
+++ javascript_full/lesson_21/2/index.js
@@ -0,0 +1,8 @@
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.log(titleElem);
+};
+const getInputElement = () => {
+  const inputElem = document.querySelector('input[type=text]');
+  console.dir(inputElem);
+};
