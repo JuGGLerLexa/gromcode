378ebed8210e21450021c157e4436d59d01ff49e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-01 15:30:57 +0300

    .

--- javascript_full/lesson_21/8/index.js
+++ javascript_full/lesson_21/8/index.js
@@ -3,19 +3,19 @@ export const finishList = () => {
   const special = document.querySelector('.special');
 
   const append = document.createElement('li');
-  append.textContent = 8;
+  append.textContent = '8';
   list.append(append);
 
   const prepend = document.createElement('li');
-  prepend.textContent = 1;
+  prepend.textContent = '1';
   list.prepend(prepend);
 
   const before = document.createElement('li');
-  before.textContent = 4;
+  before.textContent = '4';
   special.before(before);
 
   const after = document.createElement('li');
-  after.textContent = 6;
+  after.textContent = '6';
   special.after(after);
 };
 
