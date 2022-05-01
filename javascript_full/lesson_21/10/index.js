43dc2e64598a6507e2470d9d7b1fd5b68d32870d
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-01 16:42:30 +0300

    DOM

---
+++ javascript_full/lesson_21/10/index.js
@@ -0,0 +1,10 @@
+const manegeClasses = () => {
+  document.querySelector('.one').classList.add('selected');
+  document.querySelector('.two').classList.remove('selected');
+  document.querySelector('.three').classList.toggle('three_done');
+  if (document.querySelector('.four').classList.contains('some-class')) {
+    document.querySelector('.four').classList.add('another-class');
+  }
+};
+
+manegeClasses();
