7875b62cfbc5d6604b393b05d71be492046df7b6
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-01 15:26:54 +0300

    fix

--- javascript_full/lesson_21/8/index.js
+++ javascript_full/lesson_21/8/index.js
@@ -2,19 +2,19 @@ export const finishList = () => {
   const list = document.querySelector('.list');
   const special = document.querySelector('.special');
 
-  const append = document.createElement('eight');
+  const append = document.createElement('li');
   append.textContent = 8;
   list.append(append);
 
-  const prepend = document.createElement('one');
+  const prepend = document.createElement('li');
   prepend.textContent = 1;
   list.prepend(prepend);
 
-  const before = document.createElement('four');
+  const before = document.createElement('li');
   before.textContent = 4;
   special.before(before);
 
-  const after = document.createElement('six');
+  const after = document.createElement('li');
   after.textContent = 6;
   special.after(after);
 };
