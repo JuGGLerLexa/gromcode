311c42f96f0b56383e68712a373dd5d4e1171d4e
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-05-02 10:39:20 +0300

    Delete javascript_full/lesson_21/11 directory

--- javascript_full/lesson_21/11/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export const squaredNumbers = () => {
-  Array.from(document.querySelectorAll('.number')).forEach(
-    el => (el.dataset.squaredNumbers = el.dataset.number * el.dataset.number),
-  );
-};
