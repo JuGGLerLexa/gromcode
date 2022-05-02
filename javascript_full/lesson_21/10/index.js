9befb2e328894d2e78bf3e50c7fbe2d86c76199f
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-02 10:03:21 +0300

    change function

--- javascript_full/lesson_21/10/index.js
+++ javascript_full/lesson_21/10/index.js
@@ -1,8 +1,10 @@
-export const manegeClasses = () => {
+function manegeClasses() {
   document.querySelector('.one').classList.add('selected');
   document.querySelector('.two').classList.remove('selected');
   document.querySelector('.three').classList.toggle('three_done');
   if (document.querySelector('.four').classList.contains('some-class')) {
     document.querySelector('.four').classList.add('another-class');
   }
-};
+}
+
+export { manegeClasses };
