26f0750df90e583488b63dd1005843826016996e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-05 23:04:17 +0300

    addEventListener click

---
+++ javascript_full/lesson_22/5/index.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('.search__input');
+const button = document.querySelector('.search__btn');
+
+button.addEventListener('click', () => console.log(`${input.value}`));
