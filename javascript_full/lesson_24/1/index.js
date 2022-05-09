2e066e27db758028f5dc8d022a2ba6d443812e23
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-09 11:17:29 +0300

    new Date()

---
+++ javascript_full/lesson_24/1/index.js
@@ -0,0 +1,11 @@
+const weekDays = ['Sun', 'Mon', 'Thu', 'Wed'];
+
+const getDayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = getDayOfWeek(new Date(2019, 0, 1), 14);
+
+console.log(result);
