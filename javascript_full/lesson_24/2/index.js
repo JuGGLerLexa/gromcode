62e44afabec78a85956365862086a5aad9ce1b75
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-09 11:41:54 +0300

    task

--- javascript_full/lesson_24/2/index.js
+++ javascript_full/lesson_24/2/index.js
@@ -1,10 +1,19 @@
-const formatter = new Intl.DateTimeFormat('en', {
+// const formatter = new Intl.DateTimeFormat('en', {
+//   timeZone: 'UTC',
+//   hour: '2-digit',
+//   minute: '2-digit',
+//   hour12: false,
+// });
+
+// const getTime = date => formatter.format(date);
+
+// console.log(getTime(new Date()));
+
+export const formatter = new Intl.DateTimeFormat('en', {
   timeZone: 'UTC',
   hour: '2-digit',
   minute: '2-digit',
   hour12: false,
 });
 
-const getTime = date => formatter.format(date);
-
-console.log(getTime(new Date()));
+export const getGreenwichTime = date => formatter.format(date);
