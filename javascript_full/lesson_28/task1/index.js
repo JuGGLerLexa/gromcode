3716c1648d18a3bc2e248cedb914f69646f9835e
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-12 10:34:52 +0300

    calc

---
+++ javascript_full/lesson_28/task1/index.js
@@ -0,0 +1,33 @@
+// calc(3).add(2).mult(4).div(10).substract(5).result(); // -3
+
+const calc = initValue => {
+  let result = initValue;
+
+  const calculator = {
+    add(value) {
+      result += value;
+      return calculator;
+    },
+    mult(value) {
+      result *= value;
+      return this;
+    },
+    substract(value) {
+      result -= value;
+      return this;
+    },
+    div(value) {
+      result /= value;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+
+  return calculator;
+};
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+
+console.log(result);
