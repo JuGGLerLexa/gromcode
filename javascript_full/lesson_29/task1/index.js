ab941b028e823df3f68994211d9a2ec4304febe4
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-15 23:03:27 +0300

    fix

--- javascript_full/lesson_29/task1/index.js
+++ javascript_full/lesson_29/task1/index.js
@@ -3,29 +3,31 @@ export const addImage = (imgSrc, callback) => {
   imgElem.setAttribute('alt', 'My Photo');
   imgElem.src = imgSrc;
   const containerElem = document.querySelector('.page');
-
   containerElem.append(imgElem);
 
   const onImageLoaded = () => {
     callback(null, imgElem);
-    // const { width, height } = imgElem;
-    // callback(null, { width, height });
   };
 
   imgElem.addEventListener('load', onImageLoaded);
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-const imgSrc =
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
-const onImageLoaded = (error, data) => {
+// callack function
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = data;
+
+  const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
+
   sizeElem.textContent = `${width} x ${height}`;
 };
-addImage(imgSrc, onImageLoaded);
+
+// examples
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Arial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
