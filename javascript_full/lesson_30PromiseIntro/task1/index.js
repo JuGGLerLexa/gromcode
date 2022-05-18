e5357b02afbf0bad23e8530179821fff2cf8fd13
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-18 15:55:50 +0300

    promise intro, catch, new Error

--- javascript_full/lesson_30PromiseIntro/task1/index.js
+++ javascript_full/lesson_30PromiseIntro/task1/index.js
@@ -1,12 +1,11 @@
-// addImage
-
-const addImageWithPromise = imgSrc => {
+export const addImage = imgSrc => {
   const p = new Promise((resolveCb, rejectCb) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
+
+    const container = document.querySelector('.page');
+    container.append(imgElem);
 
     const onImageLoaded = () => {
       const { width, height } = imgElem;
@@ -14,36 +13,22 @@ const addImageWithPromise = imgSrc => {
     };
 
     imgElem.addEventListener('load', onImageLoaded);
-
     imgElem.addEventListener('error', () => rejectCb(new Error('Image load is failed')));
   });
 
   return p;
 };
-const imgSrc =
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const resultPromise = addImageWithPromise(imgSrc);
-
-resultPromise.catch(error => console.log(error));
-
-// console.log(result);
-
-// const onImageLoaded = (error, imgElem) => {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-
+// const onImageLoaded = imgElem => {
 //   const { width, height } = imgElem;
 //   const sizeElem = document.querySelector('.image-size');
-
 //   sizeElem.textContent = `${width} x ${height}`;
 // };
 
-// // examples
-// addImage(
+// // test call
+// const pp = addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
 //   onImageLoaded,
 // );
-
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Promise#%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5 //
+// pp.then(onImageLoaded);
+// pp.catch(error => console.log(error));
