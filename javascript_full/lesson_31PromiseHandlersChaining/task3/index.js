709ce2be77a084d7fde182ded6b32bdec3639742
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-19 15:48:04 +0300

    refactoring

--- javascript_full/lesson_31PromiseHandlersChaining/task3/index.js
+++ javascript_full/lesson_31PromiseHandlersChaining/task3/index.js
@@ -2,26 +2,11 @@ const successPromise = new Promise(resolve => {
   resolve(32);
 });
 
-/*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
- */
-
 successPromise
-  .then(number => {
-    const halfNumber = number / 2;
-    return halfNumber;
-  })
-  .then(number => {
-    /* fix this handler */
-    return number * number; // add return
-  })
-  .then(result => {
-    console.log(result); // 256
-  });
+  .then(number => number / 2)
+  .then(number => number * number)
+  .then(result => console.log(result));
 
-/*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
- */
 successPromise.then(number => number + 288).then(result => console.log(result)); // 320
 
 console.log(
