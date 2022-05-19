d2881089124199b7384f487c0ec9e3f0eb116bc1
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-19 15:45:31 +0300

    just do

---
+++ javascript_full/lesson_31PromiseHandlersChaining/task3/index.js
@@ -0,0 +1,29 @@
+const successPromise = new Promise(resolve => {
+  resolve(32);
+});
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+  .then(number => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then(number => {
+    /* fix this handler */
+    return number * number; // add return
+  })
+  .then(result => {
+    console.log(result); // 256
+  });
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise.then(number => number + 288).then(result => console.log(result)); // 320
+
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
