ee25a60a18914b6fcb2ca38a60de69b2e7e0e605
JuGGLerLexa <kudryavcev1961lexa@gmail.com>
2022-05-23 10:57:05 +0300

    in watching video

--- javascript_full/lesson_32/task2/index.js
+++ javascript_full/lesson_32/task2/index.js
@@ -7,15 +7,14 @@ const getValueWithDelay = (value, delay) =>
   });
 
 const asyncNum1 = getValueWithDelay(56, 1000);
-const asyncNum2 = getValueWithDelay(4, 2000);
-const asyncNum3 = getValueWithDelay(10, 2000);
+const asyncNum2 = getValueWithDelay(undefined, 2000);
+const asyncNum3 = getValueWithDelay('10', 3000);
 
-const getSum = numbers => {
-  numbers.reduce((acc, num) => acc + num, 0);
-};
+const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((acc, num) => acc + Number(num), 0);
 
 const asyncSum = (...asyncNumbers) => {
-  Promise.all(asyncNumbers).then(numbers => getSum(numbers));
+  return Promise.all(asyncNumbers).then(numbers => getSum(numbers));
 };
 
 // getValueWithDelay(56, 1000).then(value => console.log(value));
